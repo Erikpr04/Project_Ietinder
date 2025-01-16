f83ab14d8b13dd4beab737000cfe79c11031447a
Victor <vvalerolopez.eb@iesesteveterradas.cat>
2025-01-16 19:34:39 +0100

    [ 15 - SETUP ] Creating structure

---
+++ chat.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Victor Valero, Erik Pinto, Marta Arévalo, Pau Gracia">
+    <meta name="description" content="Esta es la vista chat de la app SwipeIt">
+
+    <script src="./js/jquery-3.7.1.min.js"></script>
+    <script src="./js/chat.js"></script>
+    <script src="https://kit.fontawesome.com/74d6337d15.js" crossorigin="anonymous"></script>
+
+    <link type="text/css" rel="stylesheet" href="./css/style.css?t=<?php echo time();?>"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">    
+
+    <title>SwipeIt! - Messages</title>
+</head>
+<body id="chat">
+    
+</body>
+</html>
