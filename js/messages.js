338950858e39c3b88f9c772def0c4c54ce0bb70e
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-01-16 14:27:26 +0100

    [FIX] minor fix part6

--- js/messages.js
+++ js/messages.js
@@ -69,7 +69,8 @@ function createProfilesDependsOfConversationStarted(data,isEmpty){
 
                     
 
-                    const messageString = lastMessageInfo[name] + ': ' + lastMessageInfo[content];
+                    const messageString = lastMessageInfo['name'] + ': ' + lastMessageInfo['content'];
+
 
                     const lastMessage = $("<p class='lastMessage'></p>").text(messageString); // contenedor que almacena los datos relacionados
                     const sectionContainer = $("<section></section>").append(name, lastMessage);
