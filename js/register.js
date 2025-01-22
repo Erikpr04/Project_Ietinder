8c299e4e026e8c5413b1627bcd50e64c2a55bb03
estillicon <paugracia86@yahoo.com>
2025-01-22 19:57:01 +0100

    mensaje

--- js/register.js
+++ js/register.js
@@ -120,7 +120,7 @@ function saveData() {
             console.log("Respuesta del servidor:", response);
             if (response.success) {
                 //alert("Registro exitoso: " + response.message);
-                createErrorTag("warning", "Registro exitoso: " + response.message);
+                createErrorTag("warning", "Registro exitoso: Tienes 48 horas para activar tu cuenta.");
                 window.location.href = "login.php"; // Redirigir a otra página si es necesario
             } else {
                 createErrorTag("error", response.message);
